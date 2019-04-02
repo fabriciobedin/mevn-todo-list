f312f6c6f69d77874df26c02b258ffe81c091957
Fabricio Bedin <fabricioobedin@gmail.com>
2019-04-01 23:20:13 -0300

    using express as middleware and cors

--- prod-server/index.js
+++ prod-server/index.js
@@ -6,16 +6,24 @@ var _express2 = _interopRequireDefault(_express);
 
 var _routes = require("./routes");
 
+var _env = require("./config/env");
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var app = (0, _express2.default)();
 
+
+(0, _env.setEnvironment)(app);
 (0, _routes.registerRoutes)(app);
 
 app.get("/", function (req, res) {
-  res.send("Hello World!");
+  if (process.env.NODE_ENV !== "production") {
+    return res.send("Running server in development mode!");
+  } else {
+    return res.sendFile("index.html", { root: __dirname + "/../dist/" });
+  }
 });
 
 app.listen(3000, function () {
-  console.log("Example app listening on port 3000!");
+  console.log("Example app listening on port 3000 in " + process.env.NODE_ENV + " mode");
 });
