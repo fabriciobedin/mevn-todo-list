e06520cf16fc7703415cb414f5ffe6a6bcef06e8
Fabricio Bedin <fabricioobedin@gmail.com>
2019-04-03 23:34:19 -0300

    validating passwords

--- dev-server/api/auth/auth-controller.js
+++ dev-server/api/auth/auth-controller.js
@@ -16,7 +16,10 @@ export function index(req, res) {
       return res.status(401).json();
     }
 
-    const passwordsMatch = true;
+    const passwordsMatch = User.passwordMatches(
+      req.body.password,
+      user.password
+    );
     if (!passwordsMatch) {
       return res.status(401).json();
     }
